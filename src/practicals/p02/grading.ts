403bdee27e4f006af0f8a61f301607d52e6cb16e
Natthawat Kittanavet <25012514amonwan@gmail.com>
2026-01-21 14:25:31 +0700

    kk

--- src/practicals/p02/grading.ts
+++ src/practicals/p02/grading.ts
@@ -1,1 +1,29 @@
 // Write your code below
+try {
+    const input_p = Number(process.argv[2]);
+    if(typeof input_p !== "number"){
+        throw new Error("Invalid input");
+    }
+    if(input_p > 0 && input_p < 100){
+        if(input_p < 50){
+            console.log("Grade is F");
+        }
+        else if(input_p < 60){
+            console.log("Grade is D");
+        }
+        else if(input_p < 70){
+            console.log("Grade is C");
+        }
+        else if(input_p < 80){
+            console.log("Grade is B");
+        }
+        else {
+            console.log("Grade is A");
+        }
+    }
+    else{
+        console.log("Invalid input");
+    }
+}catch(err){
+    console.log(err);
+}
