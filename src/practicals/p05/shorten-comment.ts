f0171ee4a942c8fdce20cfe350eb78ea223b4410
Natthawat Kittanavet <25012514amonwan@gmail.com>
2026-01-21 16:17:46 +0700

    dd

--- src/practicals/p05/shorten-comment.ts
+++ src/practicals/p05/shorten-comment.ts
@@ -1,5 +1,19 @@
 const comment = "TypeScript is a strongly typed programming language"
+const re = short
 
-function shortenComment(comment: string): string {
+export function shortenComment(comment: string): string {
   // Write your code below
+  const text: string = comment;
+  const op: string[] = text.split(" ");
+  const inf: string[] = [];
+
+  for (const d of op){
+    if(5<= d.length   && d.length <= 10){
+      inf.push(d);
+
+    }
+  }
+  let out = inf.join();
+  return out;
+  
 }
