7fe446dc4e08ec70ff1651b787698555791f9f4e
Natthawat Kittanavet <25012514amonwan@gmail.com>
2026-01-21 16:30:01 +0700

    dd

--- src/practicals/p04/number-pattern.ts
+++ src/practicals/p04/number-pattern.ts
@@ -1,6 +1,6 @@
 // Write your code below
 const inp = Number(process.argv[2]);
-/*if (typeof inp === "number" && inp > 0) {
+if (typeof inp === "number" && inp > 0) {
     
     for (let i = 0; i >= 1; i--) {
         let row = "";
@@ -9,13 +9,5 @@ const inp = Number(process.argv[2]);
         }
         console.log(row);
     }
-}*/
-let n = inp;
-for (let i = 0; i < n; i++) {
-  let row = "";
-  for (let j = 0; j < n - i; j++) {
-    row += (j + 1);
-  }
-  console.log(row);
 }
-    
+ 
